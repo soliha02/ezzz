49ae7a8edc02f905cedf33df6d0dd52e21922112
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-24 10:40:19 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,28 @@
+let num = +prompt("1dan 7 gacha son kiriting");
+
+switch (num) {
+  case 1:
+    alert("siz dushamba ni kiritingiz");
+    break;
+  case 2:
+    alert("siz seyshamba ni kiritingiz");
+    break;
+  case 3:
+    alert("siz chorshamba ni kiritingiz");
+    break;
+  case 4:
+    alert("siz payshamba ni kiritingiz");
+    break;
+  case 5:
+    alert("siz juma ni kiritingiz");
+    break;
+  case 6:
+    alert("siz shamba ni kiritingiz");
+    break;
+  case 7:
+    alert("siz yakshamba ni kiritingiz");
+    break;
+  default:
+    alert("1 dan 7 son kiritmadingiz")
+    break;
+}
